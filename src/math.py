aa1a72feae6c9b36b13aa6004d187cd99989ad73
QuangTong <quangtvtd@gmail.com>
2022-03-28 23:16:39 +0700

    test

--- src/math.py
+++ src/math.py
@@ -1,6 +1,5 @@
 def add(x: int, y: int) -> int:
     """add two numbers together
-
     Args:
                     x (int): first number to add
                     y (int): second number to add
@@ -13,7 +12,6 @@ def add(x: int, y: int) -> int:
 
 def subtract(x: int, y: int) -> int:
     """subtract one number from another
-
     Args:
                     x (int): first number to subtract
                     y (int): second number to subtract
@@ -21,7 +19,7 @@ def subtract(x: int, y: int) -> int:
     Returns:
                     int: resut of x subtract y
     """
-    
+
     return x - y
 
 
